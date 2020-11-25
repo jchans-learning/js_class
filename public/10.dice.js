f03ad0bd06a991a18273d7775927ab463f4879aa
jchans <jiangchenghans@gmail.com>
2020-11-25 16:20:13 +0800

    JS class notes

--- public/10.dice.js
+++ public/10.dice.js
@@ -5,6 +5,8 @@ const start = document.getElementById('start')
 start.addEventListener('click', function () {
   // 隨機產生一個 1~6 的整數
   const p = Math.floor(Math.random() * 6) + 1
+
+  console.log(p)
 })
 
 console.log(dice, point, start)
